076e3dde0f2a5628ff9f517c7e5d8a1d41a00a07
Martin Ouimet <mouimet@infinisoft.world>
2023-10-28 18:47:58 -0400

    added parameter filetype

--- api/index.py
+++ api/index.py
@@ -12,20 +12,19 @@ CORS(app)
 openai.api_key = os.getenv('OPENAI_KEY')
 api_key = os.getenv('OCR_KEY')
 
-def ocr_space_file(filename, overlay=False, language='fr'):
+def ocr_space_file(file, overlay=False, language='fre'):
     payload = {
         'isOverlayRequired': overlay,
         'apikey': api_key,
-        # 'language': language,
+        'filetype': file.content_type.split('/')[-1],
+        'language':language
     }
-    with open(filename, 'rb') as f:
-        r = requests.post('https://api.ocr.space/parse/image',
-                          files={filename: f},
-                          data=payload,
-                          )
-        t = r.json()
-        print(t)
-        return t["ParsedResults"][0]["ParsedText"]
+    r = requests.post('https://api.ocr.space/parse/image',
+                      files={'image': file},
+                      data=payload,
+                      )
+    t = r.json()
+    return t["ParsedResults"][0]["ParsedText"]
 
 @app.route('/smart', methods=['POST'])
 def smart():
@@ -33,10 +32,7 @@ def smart():
     if not file:
         return jsonify({'error': 'no file uploaded'}), 400
 
-    file_path = f"{file.filename}"
-
-    file.save(file_path)
-    text = ocr_space_file(file_path)
+    text = ocr_space_file(file)
 
     template = """
     Considérant les informations ci-dessous, tu va générer des notes structurées en français. Tu va formatter en utilisant du HTML et découper en section avec des paragraphes et des titres.
